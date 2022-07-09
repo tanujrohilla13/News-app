63e20d79bd1cfa28e9d0641d5c90fe2f351ec0e7
abc <abc@example.com>
2022-07-09 23:24:41 +0530

    commit

---
+++ app/src/main/java/com/example/newsapp/ui/bbc/HomeFragment.java
@@ -0,0 +1,47 @@
+package com.example.newsapp.ui.bbc;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
+
+import com.example.newsapp.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        WebView webView = root.findViewById(R.id.webViewBBC);
+        webView.loadUrl("https://www.bbc.com/");
+        webView.setWebViewClient(new WebViewClient());
+
+
+        Button btn = root.findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_nav_home_to_skyNewsFragment);
+            }
+        });
+
+        return root;
+    }
+}
---
+++ app/src/main/java/com/example/newsapp/ui/slideshow/SlideshowFragment.java
@@ -0,0 +1,36 @@
+package com.example.newsapp.ui.slideshow;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.newsapp.R;
+
+public class SlideshowFragment extends Fragment {
+
+    private SlideshowViewModel slideshowViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        slideshowViewModel =
+                ViewModelProviders.of(this).get(SlideshowViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+
+
+        WebView webView = root.findViewById(R.id.webViewCNN);
+        webView.loadUrl("https://edition.cnn.com/");
+        webView.setWebViewClient(new WebViewClient());
+
+        return root;
+    }
+}
